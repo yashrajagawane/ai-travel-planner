e87990ac0f0ed84c10005d65109b36af44611b10
yashrajagawane <agawaneyash865@gmail.com>
2026-05-16 11:26:40 +0530

    Prepared app for Render deployment

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
